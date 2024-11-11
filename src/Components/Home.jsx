62f54a491380463cf1515c34680d81844d15ece9
Jashwa <jashwa46733@gmail.com>
2024-11-11 22:55:42 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from './Sidebar'
 import Chatbox from './Chatbox'
 import './main.css'
 
 const Home = () => {
+  const [chatperson , setChatperson]=useState('Jane')
   return (
     <div className='Home'>
       <div className="container">
-        <Sidebar/>
-       <Chatbox/>
+        <Sidebar chatperson={chatperson} setChatperson={setChatperson}/>
+       <Chatbox chatperson={chatperson} setChatperson={setChatperson}/>
       </div>
     </div>
   )
